992d2a94ae11563aeccac8747ba0675eaf47f1d6
Olga Eltsova <olga.ellcova@gmail.com>
2020-10-11 18:41:23 +0500

    Правки после консультации: вынесение маг. чисел в константы, комментирование по JS Doc, др. мелкие правки

--- js/main.js
+++ js/main.js
@@ -1,22 +1,81 @@
 'use strict';
 
-const names = [`Турецкий`, `Томми`, `Кузен Ави`, `Сол`, `Микки`, `Винни`, `Фрэнки «Четыре пальца»`, `Кирпич`, `Роскошный Джордж`, `Тони «Пуля в зубах»`, `Борис «Бритва»`, `Даг «Голова»`, `Кирпич`];
+const names = [
+  `Турецкий`,
+  `Томми`,
+  `Кузен Ави`,
+  `Сол`,
+  `Микки`,
+  `Винни`,
+  `Фрэнки «Четыре пальца»`,
+  `Кирпич`,
+  `Роскошный Джордж`,
+  `Тони «Пуля в зубах»`,
+  `Борис «Бритва»`,
+  `Даг «Голова»`,
+  `Кирпич`
+];
 
-const quotes = [`Всё отлично!`, `В целом всё неплохо. Но не всё.`, `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`, `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`, `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`, `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`];
+const quotes = [
+  `Всё отлично!`,
+  `В целом всё неплохо. Но не всё.`,
+  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
+];
 
-const descriptions = [`Вот так выглядит мой отпуск!`, `Все кайфы мои`, `Тестирую новую камеру`, `Воспоминания...`, `Теперь вы знаете, что подарить мне на др`, `Не знаю, почему, но очень нравится это фото...`];
+const descriptions = [
+  `Вот так выглядит мой отпуск!`,
+  `Все кайфы мои`,
+  `Тестирую новую камеру`,
+  `Воспоминания...`,
+  `Теперь вы знаете, что подарить мне на др`,
+  `Не знаю, почему, но очень нравится это фото...`
+];
 
-let photosSrcs = [];
+const NUMBER_PHOTOS = 25;
 
-const renderPhotosSrcsArray = function (arrayLength) {
-  for (let i = 1; i <= arrayLength; i++) {
+const NumberLikes = {
+  MIN: 15,
+  MAX: 200
+}
+
+const NumberComments = {
+  MIN: 1,
+  MAX: 3
+}
+
+const NumberAvatar = {
+  MIN: 1,
+  MAX: 6
+}
+
+/**
+ * "Подбрасывает монетку" - возвращает случайное булевое значение
+ * @return {boolean} - случайное булевое значение
+ */
+const getRandomBoolean = function () {
+  return Math.random() >= 0.5;
+};
+
+/**
+ * Генерирует массив url фотографий
+ * @param {number} numberUrls - количество url фотографий
+ * @return {Array} - массив url фотографий
+ */
+const getPhotosSrcs = function (numberUrls) {
+  let photosSrcs = [];
+  for (let i = 1; i <= numberUrls; i++) {
     photosSrcs.push(`photos/` + i + `.jpg`);
   }
   return photosSrcs;
 };
-renderPhotosSrcsArray(25);
 
-/* функция к варианту создания рандомного url для фото
+/**
+ * Перемешивает массив
+ * @param {Array} array -  случайный массив
+ */
 const shuffleArray = function (array) {
   var j, swap;
   for(var i = array.length - 1; i > 0; i--) {
@@ -25,42 +84,50 @@ const shuffleArray = function (array) {
     array[j] = array[i];
     array[i] = swap;
   }
-  return array;
 }
-shuffleArray(photosSrcsArray);
-*/
 
+/**
+ * Возвращает рандомное целое число между задаваемыми min и max
+ * @param {number} min - минимальное число
+ * @param {number} max - максимальное число
+ * @return {number} - случайное число
+ */
 const getRandomNumber = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+/**
+ * Выбирает рандомный элемент из массива
+ * @param {Array} array -  случайный массив
+ * @return {*} - случайный элемент массива
+ */
 const getRandomElement = function (array) {
   return array[Math.floor(Math.random() * Math.floor(array.length))];
 };
 
-let photos = [];
+let photosSrcs = getPhotosSrcs(NUMBER_PHOTOS);
+shuffleArray(photosSrcs);
 
-const renderPhotosArray = function (photosNumber) {
+/**
+ * Генерирует массив объектов, каждый объект ‐ рандомное описание фотографии (url, описание, количество лайков, комментарии)
+ * @param {number} photosNumber - количество фотографий
+ * @return {Array} - массив объектов (фотографий)
+ */
+const getPhotos = function (photosNumber) {
+  let photos = [];
   for (let i = 0; i < photosNumber; i++) {
     photos[i] = {};
-    /*
-    вариант создания рандомного url (только вместе с функцией shuffleArray выше):
-    photos[i].url = photosSrcsArray[i];
-    */
-    let photoUrl = getRandomElement(photosSrcs);
-    photos[i].url = photoUrl;
-    let index = photosSrcs.indexOf(photoUrl);
-    photosSrcs.splice(index, 1);
+    photos[i].url = photosSrcs[i];
     photos[i].description = getRandomElement(descriptions);
-    photos[i].likes = getRandomNumber(15, 200);
+    photos[i].likes = getRandomNumber(NumberLikes.MIN, NumberLikes.MAX);
     photos[i].comments = [];
-    for (let j = 0; j < getRandomNumber(1, 3); j++) {
+    for (let j = 0; j < getRandomNumber(NumberComments.MIN, NumberComments.MAX); j++) {
       photos[i].comments[j] = {};
-      photos[i].comments[j].avatar = `img/avatar-` + getRandomNumber(1, 6) + `.svg`;
+      photos[i].comments[j].avatar = `img/avatar-` + getRandomNumber(NumberAvatar.MIN, NumberAvatar.MAX) + `.svg`;
       photos[i].comments[j].message = getRandomElement(quotes);
-      if (Math.random() >= 0.5) {
+      if (getRandomBoolean()) {
         photos[i].comments[j].message += ` ` + getRandomElement(quotes);
       }
       photos[i].comments[j].name = getRandomElement(names);
@@ -68,25 +135,30 @@ const renderPhotosArray = function (photosNumber) {
   }
   return photos;
 };
-renderPhotosArray(photosSrcs.length);
 
 const pictureTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
 
-const renderPhoto = function (photoElem) {
+/**
+ * Отрисовывает фотографии, используя данные из массива объектов - фотографий
+ * @param {number} photoElem - элемент массива фотографий (объект с описанием)
+ * @return {Object} - фрагмент кода HTML
+ */
+const renderPhoto = function (photosElem) {
   let photo = pictureTemplate.cloneNode(true);
 
-  photo.querySelector(`.picture__img`).src = photoElem.url;
-  photo.querySelector(`.picture__comments`).textContent = photoElem.comments.length;
-  photo.querySelector(`.picture__likes`).textContent = photoElem.likes;
+  photo.querySelector(`.picture__img`).src = photosElem.url;
+  photo.querySelector(`.picture__comments`).textContent = photosElem.comments.length;
+  photo.querySelector(`.picture__likes`).textContent = photosElem.likes;
 
   return photo;
 };
 
-const fragment = document.createDocumentFragment();
+const photos = getPhotos(photosSrcs.length);
 
-for (let i = 0; i < photos.length; i++) {
-  fragment.appendChild(renderPhoto(photos[i]));
-}
+const fragment = document.createDocumentFragment();
+photos.forEach(function (photo) {
+  fragment.appendChild(renderPhoto(photo));
+});
 
 const picturesContainer = document.querySelector(`.pictures`);
 picturesContainer.appendChild(fragment);
